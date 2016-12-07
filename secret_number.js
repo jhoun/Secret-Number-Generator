a60f126ee8dafd3cd9e1f24ac49577e02c776270
jhoun <jayhoun@gmail.com>
2016-12-06 17:47:18 -1000

    all test passed for closure problem

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
 'use strict';
 module.exports = function() {
-  
+
+    //Private variables
+    var number = Math.random();
+
+
+    function secretNumber() {
+      return number;
+    }
+    //Public
+    return secretNumber;
 };
+
